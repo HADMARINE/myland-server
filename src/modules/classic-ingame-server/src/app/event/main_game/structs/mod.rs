5b6a37494294aaf3b4674f7e42b14033423f9323
HADMARINE <contact@hadmarine.com>
2021-12-09 00:42:05 +0900

    WIP: basic structs

--- src/modules/classic-ingame-server/src/app/event/main_game/structs/mod.rs
+++ src/modules/classic-ingame-server/src/app/event/main_game/structs/mod.rs
@@ -1,9 +1,28 @@
 pub mod stats {
-    pub mod lands
-    pub struct User {
-
+    pub struct GlobalGameStat {}
+    // pub mod lands
+    pub struct User<LandType> {
+        pub lands: Vec<Land<LandType>>,
+        pub money: f32,
+        pub reputation: f32,
+        pub political_power: f32,
+        pub secretary: Secretary,
+        pub loans: Vec<Loan>,
+        pub effects
     }
-    
+
+    impl<T> User<T> {
+        pub fn get_total_population(&self) -> u32 {}
+    }
+
+    pub struct Secretary {}
+
+    pub struct Advertisement {}
+
+    pub struct EffectContainer {}
+
+    pub struct Effect {}
+
     pub struct Immovables {
         pub building_level: ImmovablesBuildingLevel,
         pub attractiveness: f32,
@@ -11,21 +30,20 @@ pub mod stats {
         pub productivity: f32,
     }
 
-    pub struct Land<T> {
+    pub struct Land<LandType> {
         pub tile_count: u32,
-        pub location: T
+        pub location: LandType, // Put a enum type here
     }
 
-    
-
+    pub struct Loan {
+        pub loan_bank_id: u8, // This is for distinction of ceratin bank
+    }
     pub enum ImmovablesBuildingLevel {
         Basic,
         Moderate,
         Advanced,
-        Landmark
-
+        Landmark,
     }
-    
 }
 
 pub mod status {
@@ -35,13 +53,61 @@ pub mod status {
         pub productivity: f32,
         pub population: u32,
     }
-
 }
 
 pub mod events {
     pub mod cyclic {
+        pub mod util {
+            pub struct TurnReduction {} // Wrap any events that should be resolved in certain turn
 
+            pub struct Repetitive {} // Wrap any events that should be repetitive
+        }
 
-        // pub struct 
+        use crate::app::event::main_game::{cyclic_event_queue::CyclicEvent, structs::stats::User};
+
+        pub struct PersonalEvent<LandType> {
+            pub user: User<LandType>,
+        }
+
+        impl PersonalEvent {}
+
+        impl CyclicEvent for PersonalEvent {}
+
+        pub struct GlobalEvent {
+            // pub
+        }
+
+        pub struct Income<LandType> {
+            pub user: User<LandType>,
+        }
+
+        impl Income {}
+
+        impl CyclicEvent for Income {}
+
+        pub struct Tax {}
+
+        impl CyclicEvent for Tax {}
+
+        pub struct LoanInterest {}
+
+        pub struct LoanPayment {}
+
+        pub struct PopulationTransformation {}
+
+        pub struct SpyRecruit {}
+
+        pub struct SpyActivity {}
+
+        pub struct WinLoseDetermination {}
+
+        pub struct Construction {}
+
+        pub struct Reclamation {}
+
+        pub struct Auction {}
+
+        // 기부 잠금 - user state 로 관리, 변경하는걸 이벤트
+        // pub struct
     }
 }
