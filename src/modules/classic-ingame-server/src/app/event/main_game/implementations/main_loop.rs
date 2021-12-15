fbd8d00ed95af9f0449d8dbe0cc748b16f5a0b0a
HADMARINE <contact@hadmarine.com>
2021-12-15 09:17:45 +0900

    WIP

--- src/modules/classic-ingame-server/src/app/event/main_game/implementations/main_loop.rs
+++ src/modules/classic-ingame-server/src/app/event/main_game/implementations/main_loop.rs
@@ -1,6 +1,8 @@
-use crate::app::event::main_game::lifecycle_manager::turnloop::{
+use std::time::Instant;
+
+use crate::app::event::main_game::{lifecycle_manager::turnloop::{
     TurnLoopLifecycleManager, TurnLoopStatus,
-};
+}, constants::TIME_PER_TURN_SEC};
 
 pub fn main_loop() {
     let tlm = TurnLoopLifecycleManager::new();
@@ -8,11 +10,19 @@ pub fn main_loop() {
     tlm.set_event_handler(TurnLoopStatus::EndTurnWait, end_turn_wait);
     tlm.set_event_handler(TurnLoopStatus::WaitUntilCue, wait_until_cue);
 
-    tlm.tlm.kickoff();
+    tlm.kickoff();
 }
 
-fn turn_running() {}
+fn turn_running(tlm: &TurnLoopLifecycleManager) {
+    let end_turn_ctrl = move || {
+        let start_time = Instant::now();
+        while start_time.elapsed().as_secs() < TIME_PER_TURN_SEC {}
+        tlm.end_turn();
+    }
+}
 
-fn end_turn_wait() {}
+fn end_turn_wait(tlm: &TurnLoopLifecycleManager) {
+    tlm.new_turn();
+}
 
-fn wait_until_cue() {}
+fn wait_until_cue(tlm: &TurnLoopLifecycleManager) {}
