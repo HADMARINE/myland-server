90a26b6d78956ba5751f3ed351d724f795038108
HADMARINE <contact@hadmarine.com>
2022-01-07 01:07:04 +0900

    WIP

--- src/modules/classic-ingame-server/src/app/event/main_game/event_handler/mod.rs
+++ src/modules/classic-ingame-server/src/app/event/main_game/event_handler/mod.rs
@@ -11,6 +11,7 @@ pub fn register(ctrl: ChannelController) -> Result<Option<JsonValue>, Box<QuickS
     let u_uuid = (ctrl.accepted_client as ChannelClient).uid;
 
     if !ctrl.value.is_object() {
+        return Err(QuickSocketError::JsonParseFail.to_box());
         // TODO : Error handle
     }
 
@@ -23,15 +24,17 @@ pub fn register(ctrl: ChannelController) -> Result<Option<JsonValue>, Box<QuickS
     let userid = match mapped_data.get("userid") {
         Some(v) => v.to_string(),
         None => {
-            // TODO : Error
+            return Err(QuickSocketError::JsonFormatInvalid.to_box());
         }
     };
     let token = match mapped_data.get("token") {
         Some(v) => v.to_string(),
         None => {
-            // TODO : Error
+            return Err(QuickSocketError::JsonFormatInvalid.to_box());
         }
     };
+
+    ctrl.channel.;
 }
 
 pub fn user_ready(ctrl: ChannelController) -> Result<Option<JsonValue>, Box<QuickSocketError>> {
